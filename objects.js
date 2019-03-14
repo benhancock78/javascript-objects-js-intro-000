a5a2dd36a49a64820db547d6e562edd611789ab6
Ben Hancock <benjaminhancock@hotmail.co.uk>
2019-03-14 14:57:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -10,6 +10,6 @@ function  updatePlaylist (playlist, artistName, songTitle){
 }
 
 function  removeFromPlaylist(artistName, playlist)  {
-  delete playlist['artistName'];
+  delete playlist[artistName];
   return  playlist;
 }
